a9b59a913316909c5a254050b904e3046ed01a66
Wander <micropelis@gmail.com>
2025-10-20 22:42:42 -0300

    Barra lateral

---
+++ src/container/BarraLateral/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  background-color: #eee;
+  height: 100vh;
+`
+
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 8px;
+  margin-top: 16px;
+`
+
+export const Campo = styled.input`
+  border-radius: 8px;
+  border: 1px solid #a1a1a1;
+  padding: 8px;
+  width: 100%;
+  font-weight: bold;
+  color: #5e5e5e;
+`
